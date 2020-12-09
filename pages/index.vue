2f01f9842fb6e1c9696352fd6bbf025fd856548f
hosein <hosein.asgari2014@gmail.com>
2020-12-09 17:18:19 +0330

    d

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,8 @@
         test
       </h1>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
+ 
+ 
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
